fc1e66b4b214bb16abd2d8102addd873af449834
José Augusto <joseaugusto0408@gmail.com>
2023-06-07 12:07:05 -0300

    estrutura inicial e css global

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Post } from "./Post";
+
+export function App() {
+
+  return (
+  <>
+    <Post />
+  </>
+  )
+}
+
---
+++ src/Post.jsx
@@ -0,0 +1,7 @@
+export function Post(){
+    return (
+        <>
+            <strong>Post</strong>
+        </>
+    )
+}
