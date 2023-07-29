e36943ac19a6518da7ab0e5f2f2e42429b49c91c
José Augusto <joseaugusto0408@gmail.com>
2023-07-29 07:42:51 -0300

    interando JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,40 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from "./components/Sidebar.jsx";
 
+const posts = [
+  {
+    id: 1,
+  author: {
+    avatarUrl:'https://github.com/joseaugustoss.png',
+    name:'José Augusto',
+    role: 'CTO @hipocode'
+  },
+    content:[
+      
+      {type:'paragraph',content: 'Fala galeraa 👋 '},
+      {type:'paragraph',content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type:'link',content:' jane.design/doctorcare</a>'}
+      
+    ],
+    publishedAt: new Date('2023-07-29 08:10:05')
+  },
+  {
+    id: 2,
+  author: {
+    avatarUrl:'https://github.com/diego3g.png',
+    name:'Diego Fernandes',
+    role: 'CTO @rocketseat'
+  },
+    content:[
+      
+      {type:'paragraph',content: 'Fala galeraa 👋 '},
+      {type:'paragraph',content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type:'link',content:' jane.design/doctorcare</a>'}
+      
+    ],
+    publishedAt: new Date('2023-07-30 13:10:05')
+  }
+]
 export function App() {
 
   return (
@@ -12,14 +46,15 @@ export function App() {
     <div className={styles.wrapper}> 
         <Sidebar/>
         <main>
-          <Post 
-            author="José Augusto"
-            content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
-          />
-          <Post 
-            author="José Augusto"
-            content="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
 
         </main>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,9 @@
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
-export function Post(){
+
+export function Post(props){
+  console.log(props)
   return (
   <article className={styles.post}>
    <header>
@@ -15,14 +17,7 @@ export function Post(){
         <time title="29 de junho de 2023" dateTime="2023-06-29 14:05:04">Publicado há 1h</time>
    </header> 
       <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
-        <p><a href="#"> 👉 {' '} jane.design/doctorcare</a></p>
-        <p>
-          <a href="#"> #novoprojeto </a> {' '}
-          <a href="#"> #nlw </a>{' '}
-          <a href="#"> #rocketseat </a> {' '}
-          </p>
+        
       </div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
