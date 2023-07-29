7ac2a2f6524b72d2c23105223923ca07c1fab2c9
José Augusto <joseaugusto0408@gmail.com>
2023-07-29 06:56:22 -0300

    componente avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import {PencilLine} from '@phosphor-icons/react';
 
@@ -9,7 +10,7 @@ export function Sidebar(){
       src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" 
      /> 
       <div className={styles.profile} >
-        <img className={styles.avatar} src="https://github.com/joseaugustoss.png" alt="José Augusto" />
+        <Avatar src="https://github.com/joseaugustoss.png"/>
         <strong>José Augusto</strong>
         <span>Web Developer</span>
       </div>
